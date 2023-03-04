ee08fe491b46ce2a04ba94f7287e16d1f83e67d4
Cheolsoon <mn5724@naver.com>
2023-03-04 11:20:56 +0900

    [BOJ] 7576 토마토

--- java/java_algorithm/src/Main_7576_토마토.java
+++ java/java_algorithm/src/Main_7576_토마토.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main_7576_토마토 {
@@ -13,7 +15,8 @@ public class Main_7576_토마토 {
 	 * 최소일수
 	 * */
 	static int N,M, graph[][], maxCnt,cnt, time;
-	
+	static int dx[] = {-1,1,0,0};
+	static int dy[] = {0,0,-1,1};
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(in.readLine());
@@ -21,6 +24,7 @@ public class Main_7576_토마토 {
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
 		graph = new int[N][M];
+		Queue<int[]> queue = new LinkedList<>();//익은 토마토의 정보 저장
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(in.readLine());
@@ -28,20 +32,36 @@ public class Main_7576_토마토 {
 				// 익은 토마토 1, 익지않은 토마토 0, -1 토마토 없음
 				graph[i][j] = Integer.parseInt(st.nextToken());
 				if(graph[i][j] == 0) maxCnt++;
+				if(graph[i][j]==1) //익은 토마토
+					  queue.add(new int[] {i,j});
 			}
 		}
 		
-		while(true) {
+		while(!queue.isEmpty()) {
 			if(cnt==maxCnt) break;
-//			if(check()) break;
-			// 토마토 확산
-			if(!spread()) {// 변한 토마토가 없다면
-				time=-1;
-				break;
-			};
-			time++;
+
+			int[] p = queue.poll();
+			int x = p[0];
+			int y = p[1];
+
+			for(int i=0; i<4; i++) {
+				int nx = x+dx[i];
+				int ny = y+dy[i];
+				
+				if(nx < 0 || nx >= N || ny < 0 || ny >= M)
+					continue;
+				if( graph[nx][ny] == 0) {// 익지 않은 토마토 
+					graph[nx][ny] = graph[x][y] + 1;
+					queue.offer(new int[] {nx,ny});
+					cnt++;
+				}
+			}
 		}
-		System.out.println(time);
+		if(!check()) {
+			System.out.println(-1);
+		}else {
+			System.out.println(time-1);
+		}
 	}
 
 	private static boolean check() {
@@ -50,44 +70,13 @@ public class Main_7576_토마토 {
 				if(graph[i][j] == 0) {
 					return false;
 				}
+				if(graph[i][j] >= 1) {
+					time = Math.max(time, graph[i][j]);
+				}
 			}
 		}
 		return true;
 	}
 
-	private static boolean spread() {
-		int dx[] = {-1,1,0,0};
-		int dy[] = {0,0,-1,1};
-		int newGraph[][] = new int[N][M];
-		boolean isChange = false;
-		for (int i = 0; i < N; i++) {
-			newGraph[i] = Arrays.copyOf(graph[i], graph[i].length);
-		}
-		
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if(graph[i][j] == 1) {//익은토마토면 확산
-					int nx,ny;
-					for (int d = 0; d < 4; d++) {
-						nx = i+dx[d];
-						ny = j+dy[d];
-						if(nx>=0 && nx<N && ny>=0 && ny<M) {
-							if(newGraph[nx][ny] == 0) {
-								newGraph[nx][ny] = 1;
-								isChange=true;
-								cnt++;
-							}
-						}
-					}
-				}
-			}
-		}
-		
-		for (int i = 0; i < N; i++) {
-			graph[i] = Arrays.copyOf(newGraph[i], newGraph[i].length);
-		}
-		return isChange;
-	}
 
 }
