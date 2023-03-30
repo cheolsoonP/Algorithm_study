bb526e051de9f14ef7f0292de76f8d749464b47d
Park Cheolsoon <mn5724@naver.com>
2023-03-30 15:25:42 +0900

    [Unrated] Title: 녹색 옷 입은 애가 젤다지?, Time: 228 ms, Memory: 18952 KB -BaekjoonHub

---
+++ 백준/Unrated/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int T, n, graph[][], dp[][], result;
+	static int dx[] = {-1,1,0,0};
+	static int dy[] = {0,0,-1,1};
+	static ArrayDeque<int[]> q;
+	static boolean visited[][];
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		int cnt = 0;
+		while(true) {
+			cnt++;
+			n = Integer.parseInt(in.readLine());
+			if(n == 0) break;
+			graph = new int[n][n];
+			dp = new int[n][n];
+			
+			PriorityQueue<int[]> pq = new PriorityQueue<>((o1,o2)->{
+                return o1[0]-o2[0];
+            });
+			
+			visited = new boolean[n][n];
+			
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(in.readLine());
+				for(int j=0;j<n;j++) {
+					graph[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}			
+			for(int i=0;i<n;i++)
+				Arrays.fill(dp[i], Integer.MAX_VALUE);
+
+			pq.add(new int[] {graph[0][0], 0,0});
+			dp[0][0] = graph[0][0];
+
+			visited[0][0] = true;
+			
+			while(!pq.isEmpty()) {
+				int temp[] = pq.peek();
+				int dist = temp[0];
+				int x = temp[1], y = temp[2];
+				pq.poll();
+				if(dp[x][y] < dist) continue;
+				for (int i = 0; i < 4; i++) {
+					int nx = x+dx[i];
+					int ny = y+dy[i];
+					if(nx<0||nx>=n||ny<0||ny>=n) continue;
+					if(dp[nx][ny]<=dist+graph[nx][ny]) continue;
+					dp[nx][ny] = dist+graph[nx][ny];
+					pq.add(new int[] {dp[nx][ny], nx,ny});
+				}
+			}
+			result = Math.min(dp[n-1][n-2], dp[n-2][n-1]) + graph[n-1][n-1];
+			sb.append("Problem "+cnt+": ").append(result).append('\n');
+		}
+		System.out.println(sb);
+	}
+
+}
