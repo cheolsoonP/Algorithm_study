cc59771183b1c58b7bc3ad6868115d0031a45f25
Park Cheolsoon <mn5724@naver.com>
2023-04-12 15:00:17 +0900

    [Gold IV] Title: 타임머신, Time: 340 ms, Memory: 19404 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.java
@@ -0,0 +1,85 @@
+import java.util.*;
+import java.io.*;
+/*
+ * dist 출발지로부터 최단거리 저장
+ * 출발지는 0 나머지는 INF로 초기화
+ * 1. 간선 m개  하나씩 모두 살펴본다
+ *  현재 가중치 cost v, w -> v에서 W로 가는 비용
+ *  dist[v] - 현재까지 V까지 최소거리
+ *  dist[v] 무한대가 아니면 -> 2 진행
+ * 2. dist[v] = min (dist[v], dist[w] + cost(w, v) )
+ * */
+
+public class Main {
+	static class Edge {
+		int from;
+		int to;
+		int cost;
+		
+		public Edge(int from, int to, int cost) {
+			super();
+			this.from = from;
+			this.to = to;
+			this.cost = cost;
+		}
+	}
+	static int N, M;
+	static long dist[];
+	static List<Edge> edgeList;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		st = new StringTokenizer(in.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		edgeList = new ArrayList<>();
+
+		dist = new long[N+1];
+		Arrays.fill(dist, Long.MAX_VALUE);
+		dist[1] = 0;
+		
+		for(int i=0;i<M;i++) {
+			st = new StringTokenizer(in.readLine());
+			int from = Integer.parseInt(st.nextToken());
+			int to = Integer.parseInt(st.nextToken());
+			int cost = Integer.parseInt(st.nextToken());
+			edgeList.add(new Edge(from, to, cost));
+		}
+		
+		
+		for(int i=1;i<=N;i++) {
+			for(Edge edge: edgeList) {
+				if(dist[edge.from] == Long.MAX_VALUE) continue;
+				
+				if(dist[edge.to] > dist[edge.from] + edge.cost) {
+					dist[edge.to] = dist[edge.from]+edge.cost;
+					if(i == N) {
+						System.out.println("-1");
+						return;
+					}
+					
+				}
+			}
+		}
+		
+		for(int i=2;i<=N;i++) {
+			if(dist[i] == Long.MAX_VALUE) {
+				System.out.println("-1");
+			}else {
+				System.out.println(dist[i]);
+			}
+		}
+	}
+}
+/*
+ * 1번째 시도 - 플로이드 워셜
+ *  -> 음의 사이클이 존재할 수 있기 때문에 불가능하다.
+ *  -> 음의 싸이클 판단 가능 -> 0으로 초기화 해준 부분 -> -가 되면 음의 사이클 존재
+ *  -> 
+ * 2번째 시도 - 벨만 포드 
+ * 	음의 가중치를 가진 간선 -> 벨만 포드 or 플로이드 워셜 
+ * 	출발지가 하나의 정점.
+ * 	
+ * */
