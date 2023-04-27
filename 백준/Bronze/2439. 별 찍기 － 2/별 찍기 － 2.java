5eda25d4059b918b5cde8292cf5c9aed85fc11c7
Park Cheolsoon <mn5724@naver.com>
2023-04-27 23:43:30 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 216 ms, Memory: 17816 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		for(int i=1;i<=N;i++) {
+			for(int j=N-i;j>0;j--) {
+				sb.append(" ");
+			}
+			for(int j=1;j<=i;j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
