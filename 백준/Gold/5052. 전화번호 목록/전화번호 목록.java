caa578e6fc3f9b649ae69b50b29910741c3e3b08
Park Cheolsoon <mn5724@naver.com>
2023-04-30 14:02:31 +0900

    [Gold IV] Title: 전화번호 목록, Time: 468 ms, Memory: 166348 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,7 +1,22 @@
 import java.io.*;
 import java.util.*;
 
+// 방법 3: Trie 자료구조
 public class Main {
+	static class TrieNode {
+		boolean isEndWord;
+		TrieNode[] children = new TrieNode[10];
+		
+		public TrieNode() {
+			isEndWord = false;
+			children = new TrieNode[10];
+			for(int i=0;i<=9;i++) {
+				children[i] = null;
+			}
+		}
+	}
+	
+	static TrieNode root;//root 노드 설정
 	static int N;
 	
 	public static void main(String[] args) throws Exception {
@@ -10,33 +25,56 @@ public class Main {
 		int T = Integer.parseInt(in.readLine());
 		for(int tc=1;tc<=T;tc++) {
 			N = Integer.parseInt(in.readLine());
+			root = new TrieNode();
 			String num[] = new String[N];
-			Map<String, Integer> map = new HashMap<>();
 			
 			for(int i=0;i<N;i++) {
 				num[i] = in.readLine();
-				map.put(num[i], 1);
+				insert(num[i]);
 			}
-			
-			// Arrays.sort(num);
-			if(isConsistent(num, map)) {
-				sb.append("YES\n");
-			}else {
-				sb.append("NO\n");
+
+			for(int i=0;i<N;i++) {
+				if(!available(num[i])) {
+					sb.append("NO\n");
+					break;
+				}
+				if(i==N-1) {
+					sb.append("YES\n");
+				}
 			}
 		}
 		
 		System.out.println(sb);
 		in.close();
 	}
-
-	private static boolean isConsistent(String[] num, Map<String, Integer> map) {
-		for(int i=0;i<N;i++) {
-			for(int j=1;j<num[i].length();j++) {
-				if(map.containsKey(num[i].substring(0, j))) {
-					return false;
-				}
+	
+	private static void insert(String str) {
+		TrieNode curr = root;
+		int length = str.length();
+		int level;
+		int index;
+		
+		for(level=0;level<length;level++) {
+			index = str.charAt(level) - '0';
+			if(curr.children[index] == null) {
+				curr.children[index] = new TrieNode();				
 			}
+			curr = curr.children[index];
+		}
+		curr.isEndWord = true;
+	}
+	
+	private static boolean available(String str) {
+		TrieNode curr = root;
+		int length = str.length();
+		int level;
+		int index;
+		
+		for(level=0;level<length;level++) {
+			index = str.charAt(level) - '0';
+			if(curr.isEndWord)
+				return false;
+			curr = curr.children[index];
 		}
 		return true;
 	}
