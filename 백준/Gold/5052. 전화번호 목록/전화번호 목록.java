2a637276b6d17431d45ebade1249ece316c2cfaa
Park Cheolsoon <mn5724@naver.com>
2023-04-30 13:27:21 +0900

    [Gold IV] Title: 전화번호 목록, Time: 608 ms, Memory: 100120 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -11,13 +11,15 @@ public class Main {
 		for(int tc=1;tc<=T;tc++) {
 			N = Integer.parseInt(in.readLine());
 			String num[] = new String[N];
+			Map<String, Integer> map = new HashMap<>();
 			
 			for(int i=0;i<N;i++) {
 				num[i] = in.readLine();
+				map.put(num[i], 1);
 			}
 			
-			Arrays.sort(num);
-			if(isConsistent(num)) {
+			// Arrays.sort(num);
+			if(isConsistent(num, map)) {
 				sb.append("YES\n");
 			}else {
 				sb.append("NO\n");
@@ -28,10 +30,12 @@ public class Main {
 		in.close();
 	}
 
-	private static boolean isConsistent(String[] num) {
-		for(int i=0;i<N-1;i++) {
-			if(num[i+1].startsWith(num[i])) {
-				return false;
+	private static boolean isConsistent(String[] num, Map<String, Integer> map) {
+		for(int i=0;i<N;i++) {
+			for(int j=1;j<num[i].length();j++) {
+				if(map.containsKey(num[i].substring(0, j))) {
+					return false;
+				}
 			}
 		}
 		return true;
