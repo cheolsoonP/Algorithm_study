8b8a9540ab9e182992384e20c49251fc664fcc27
Park Cheolsoon <mn5724@naver.com>
2023-06-08 13:05:29 +0900

    [Silver II] Title: 이동하기, Time: 492 ms, Memory: 78584 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -2,10 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
 	static int N, M, graph[][], dp[][];
-	// 우 하 우하
-	static int dx[] = {0,1,1};
-	static int dy[] = {1,0,1};
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -14,39 +12,20 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		graph = new int[N][M];
+		graph = new int[N+1][M+1];
 		dp = new int[N+1][M+1];
-		for(int i=0;i<N;i++) {
+		for(int i=1;i<=N;i++) {
 			st = new StringTokenizer(in.readLine());
-			for(int j=0;j<M;j++) {
+			for(int j=1;j<=M;j++) {
 				graph[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		dp[0][0] = graph[0][0];
-		Deque<int[]> q = new ArrayDeque<>();
-		q.add(new int[] {0,0});
-		boolean visit[][];
-		visit = new boolean[N][M];
-		
-		while(!q.isEmpty()) {
-			int x = q.peekFirst()[0];
-			int y = q.pollFirst()[1];
-			for(int i=0;i<2;i++) {
-				int nx = x+dx[i];
-				int ny = y+dy[i];
-				if(nx<0||nx>=N||ny<0||ny>=M) continue;
-				if(dp[nx][ny] < dp[x][y] + graph[nx][ny]) {
-					dp[nx][ny] = dp[x][y]+graph[nx][ny];
-					q.add(new int[] {nx,ny});					
-				}else if(dp[nx][ny] == dp[x][y] + graph[nx][ny]) {
-					if(!visit[nx][ny]) {
-						visit[nx][ny] = true;
-						q.add(new int[] {nx,ny});
-					}
-				}
+		for(int i=1;i<=N;i++) {
+			for(int j=1;j<=M;j++) {
+				dp[i][j] = Math.max(Math.max(dp[i-1][j-1], dp[i-1][j]), dp[i][j-1])+graph[i][j];
 			}
 		}
 		
-		System.out.println(dp[N-1][M-1]);
+		System.out.println(dp[N][M]);
 	}
 }
