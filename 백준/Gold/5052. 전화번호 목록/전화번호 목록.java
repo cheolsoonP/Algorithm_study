553a41e100cd0cab28401fd03d5c58a5c01a2b89
Park Cheolsoon <mn5724@naver.com>
2023-09-24 23:29:35 +0900

    [Gold IV] Title: 전화번호 목록, Time: 676 ms, Memory: 34712 KB -BaekjoonHub

--- 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.java
@@ -1,22 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-// 방법 3: Trie 자료구조
 public class Main {
-	static class TrieNode {
-		boolean isEndWord;
-		TrieNode[] children = new TrieNode[10];
-		
-		public TrieNode() {
-			isEndWord = false;
-			children = new TrieNode[10];
-			for(int i=0;i<=9;i++) {
-				children[i] = null;
-			}
-		}
-	}
-	
-	static TrieNode root;//root 노드 설정
 	static int N;
 	
 	public static void main(String[] args) throws Exception {
@@ -25,56 +10,29 @@ public class Main {
 		int T = Integer.parseInt(in.readLine());
 		for(int tc=1;tc<=T;tc++) {
 			N = Integer.parseInt(in.readLine());
-			root = new TrieNode();
 			String num[] = new String[N];
 			
 			for(int i=0;i<N;i++) {
 				num[i] = in.readLine();
-				insert(num[i]);
 			}
-
-			for(int i=0;i<N;i++) {
-				if(!available(num[i])) {
-					sb.append("NO\n");
-					break;
-				}
-				if(i==N-1) {
-					sb.append("YES\n");
-				}
+			
+			Arrays.sort(num);
+			if(isConsistent(num)) {
+				sb.append("YES\n");
+			}else {
+				sb.append("NO\n");
 			}
 		}
 		
 		System.out.println(sb);
 		in.close();
 	}
-	
-	private static void insert(String str) {
-		TrieNode curr = root;
-		int length = str.length();
-		int level;
-		int index;
-		
-		for(level=0;level<length;level++) {
-			index = str.charAt(level) - '0';
-			if(curr.children[index] == null) {
-				curr.children[index] = new TrieNode();				
-			}
-			curr = curr.children[index];
-		}
-		curr.isEndWord = true;
-	}
-	
-	private static boolean available(String str) {
-		TrieNode curr = root;
-		int length = str.length();
-		int level;
-		int index;
-		
-		for(level=0;level<length;level++) {
-			index = str.charAt(level) - '0';
-			if(curr.isEndWord)
+
+	private static boolean isConsistent(String[] num) {
+		for(int i=0;i<N-1;i++) {
+			if(num[i+1].startsWith(num[i])) {
 				return false;
-			curr = curr.children[index];
+			}
 		}
 		return true;
 	}
