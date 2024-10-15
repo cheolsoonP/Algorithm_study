46e294eccd7e6b2c01cbe7e7d81c6565667ee937
Park Cheolsoon <mn5724@naver.com>
2024-10-15 09:48:37 +0900

    [Silver III] Title: N과 M (1), Time: 332 ms, Memory: 33700 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,51 +1,43 @@
-import java.io.*;
-import java.util.*;
+import java.io.InputStream;
 
 public class Main {
-	static int N,M;
-	static StringBuilder sb = null;
+	static StringBuilder sb = new StringBuilder();
+	static int n;
+	static int m;
+	static int [] isChecked;
+	static int [] arr;
 	
 	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        InputStream in = System.in;
+		n = in.read() - '0';
+		in.read();
+		m = in.read() - '0';
 		
-		StringTokenizer st = new StringTokenizer(in.readLine());
+		isChecked = new int[n+1];
+		arr = new int[m];
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		dfs(0);
 		
-		boolean [] visited = new boolean[N+1];
-		int[] arr = new int[M];
+		System.out.println(sb.toString());
+	}
+	public static void dfs(int cnt) {
+		// 종료 조건
+		if (cnt == m) {
+			for (int i : arr)
+				sb.append(i+" ");
+			sb.append('\n');
+			return;
+		}
 		
-		for (int i=1;i<=N; i++) {
-			int cnt = 0;
-			visited[i] = true;
+		for (int i = 1; i < n+1; i++) {
+			if (isChecked[i] == 1)
+				continue;
+			isChecked[i] = 1;
 			arr[cnt] = i;
-			backTracking(visited, cnt+1, arr);
-			visited[i] = false;
+			
+			dfs(cnt+1);
+			
+			isChecked[i] = 0;
 		}
 	}
-	
-	private static void backTracking(boolean[] visited, int cnt, int[] arr) {
-		if (cnt >= M) {
-			sb = new StringBuilder();
-			for (int i=0;i<cnt;i++) {
-				sb.append(arr[i]).append(" ");
-			}
-			System.out.println(sb.toString());
-			return; 
-		}
-		
-		for (int i=1;i<=N;i++) {
-			if (visited[i]) continue;
-			visited[i] = true;
-			arr[cnt] = i;
-//			System.out.println("cnt: "+cnt);
-//			System.out.println(Arrays.toString(arr));
-			backTracking(visited, cnt+1, arr);
-			visited[i] = false;	
-		}
-		
-		
-	}
-	
 }
