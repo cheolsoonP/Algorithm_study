b5582938eb5d37840b460ec2e84b2b1212684bc5
Park Cheolsoon <mn5724@naver.com>
2024-10-15 15:29:06 +0900

    [Gold III] Title: 감시, Time: 732 ms, Memory: 282584 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -1,169 +1,154 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	static int N,M,res, graph[][], newGraph[][];
-	// 상 우 하 좌
-	static int dx[] = {-1, 0, 1, 0};
-	static int dy[] = {0, 1, 0, -1};
-	static List<Integer[]> camera;
+	static int N, M; 
+	static int[][] originalMap, tempMap;
+	static List<int[]> cam;
+	
+	// 상우하좌
+	static int[] dx = {-1,0,1,0};
+	static int[] dy = {0,1,0,-1};
+	static int[] dir;
+	static int minCnt = Integer.MAX_VALUE;
+	
 	
 	public static void main(String[] args) throws Exception {
+		/* N * M 사무실 
+		 * K개 CCTV 
+		 * CCTV는 5가지 종류
+		 * 1방향, 좌우, 상좌, 좌상우, 상하좌우 
+		 * 해당 방향 칸 전체 감시 가능 
+		 * CCTV는 벽 통과 불가능 
+		 * 0은 빈 칸, 6은 벽, 1~5는 CCTV의 번호
+		 * 사무실의 크기와 상태, 
+		 * 그리고 CCTV의 정보가 주어졌을 때, 
+		 * CCTV의 방향을 적절히 정해서, 
+		 * 사각 지대의 최소 크기를 구하는 프로그램을 작성하시오.
+		 * 
+		 * 1번 -> 1방향으로만 봄, 4방향 회전 가능 
+		 * 2번 -> 상하 or 좌우 
+		 * 3번 -> 좌상, 상우, 우하, 하좌 
+		 * 4번 -> 좌상우, 상우하, 우하좌, 하좌상
+		 * 5번 -> 상하좌우
+		*/
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new  StringBuilder();
+		
 		StringTokenizer st = null;
-		
 		st = new StringTokenizer(in.readLine());
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		camera = new ArrayList<>(); //카메라 1번~5번 담는다. 
-		graph = new int[N][M];
-		res = Integer.MAX_VALUE;
-		
-		for (int i = 0; i < N; i++) {
+		tempMap = new int[N][M];
+		originalMap = new int[N][M];
+		cam = new ArrayList<>();
+
+		for (int i=0;i<N;i++) {
 			st = new StringTokenizer(in.readLine());
-			for (int j = 0; j < M; j++) {
-				graph[i][j] = Integer.parseInt(st.nextToken());
-				if (graph[i][j] >= 1 && graph[i][j] <= 5) {
-					camera.add(new Integer[] {graph[i][j], i, j});
-				} 
-			}
-		}
-				
-		if (camera.size() > 0) dfs(0, new ArrayList<>());
-		else {
-			int cnt = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (graph[i][j] == 0) {
-						cnt++;
-					}
+			for (int j=0;j<M;j++) {
+				originalMap[i][j] = Integer.parseInt(st.nextToken());
+				if (originalMap[i][j] >= 1 && originalMap[i][j] <= 5) {
+					cam.add(new int[] {i,j,originalMap[i][j]});
 				}
 			}
-			res = cnt;
 		}
-		System.out.println(res);
+		dir = new int[cam.size()];
+		
+		dfs(0);
+		
+		System.out.println(minCnt);
 	}
-	
-	// look : dir, x,y 을 담음 -> 감시 방향 위치 리스트
-	// DFS 각 카메라별 가능한 방향 설정 후 다음 카메라로 넘김 
-	private static void dfs(int idx, List<Integer[]> look) { // 카메라 번호, 
-		if (idx == camera.size()) {
-			newGraph = new int[N][M];
-			// 복사			
-			for (int i = 0; i < N; i++) {
-				newGraph[i] = Arrays.copyOf(graph[i], M);				
-			}
-						
-			// 방향들 감시			
-			for (int i = 0; i < look.size(); i++) {
-				Integer[] tmp = look.get(i);
-				bfs(tmp[0], tmp[1], tmp[2]);
-			}
-			// 사각지대 계산
-			int resTmp = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < M; j++) {
-					if (newGraph[i][j] == 0) { // 사각지대
-						resTmp++;
-					}
-				}
-			}
-			
-			
-			res = Math.min(res, resTmp);
+
+	private static void dfs(int idx) {
+		if (idx == cam.size()) {
+//			System.out.println("cam : "+cam.size());
+//			System.out.println("idx : "+idx);
+//
+//			System.out.println("dir[idx] :"+Arrays.toString(dir));
+//			
+			simulation();
+			count();
 			return;
 		}
 		
+		for (int i=0;i<4;i++) {
+			dir[idx] = i;
+			dfs(idx+1);
+		}
+	}
 
-		Integer[] temp = camera.get(idx);
-		int camType = temp[0];
-		int x = temp[1];
-		int y = temp[2];
-		
-		switch (camType) {
-		case 1: // 한방향
-			for (int i = 0; i < 4; i++) {
-				look.add(new Integer[] {i,x,y});
-				dfs(idx+1, look);
-				look.remove(look.size()-1);
+	private static void count() {
+		int cnt = 0;
+		for (int i=0;i<N;i++) {
+			for (int j=0;j<M;j++) {
+				if(tempMap[i][j] == 0) {
+					cnt++;
+				}
 			}
-			break;
-		case 2: // 양방향 서로반대
-			for (int i = 0; i < 2; i++) {
-				look.add(new Integer[] {i,x,y});
-				look.add(new Integer[] {i+2,x,y});
-				dfs(idx+1, look);
-				look.remove(look.size()-1);
-				look.remove(look.size()-1);				
+		}
+		if (minCnt > cnt) {
+			minCnt = cnt;
+		}
+	}
+
+	private static void simulation() {
+		copyMap();
+		for (int i=0;i<cam.size();i++) {
+			int[] temp = cam.get(i);
+			int x = temp[0];
+			int y = temp[1];
+			int camNum = temp[2];
+			
+			if (camNum == 1) {
+				spread(x,y,dir[i]);
+			} else if (camNum == 2) {
+				if (dir[i] <= 1) {
+					// 상하
+					spread(x,y,0);
+					spread(x,y,2);
+				} else {
+					// 좌우
+					spread(x,y,1);
+					spread(x,y,3);
+				}
+			} else if (camNum == 3) {
+				spread(x,y,dir[i]);
+				spread(x,y,dir[i]+1);
+			} else if (camNum == 4) {
+				spread(x,y,dir[i]);
+				spread(x,y,dir[i]+1);
+				spread(x,y,dir[i]+2);
+			} else if (camNum == 5) {
+				spread(x,y,dir[i]);
+				spread(x,y,dir[i]+1);
+				spread(x,y,dir[i]+2);
+				spread(x,y,dir[i]+3);
 			}
-			break;
-		case 3: // 양방향 서로 직각 - 4방향
-			for (int i = 0; i < 4; i++) {
-				look.add(new Integer[] {i,x,y});
-				look.add(new Integer[] {(i+1)%4,x,y});
-				dfs(idx+1, look);
-				look.remove(look.size()-1);
-				look.remove(look.size()-1);
-			}
-			break;
-		case 4: // 3방향 - > 4방향
-			for (int i = 0; i < 4; i++) {
-				look.add(new Integer[] {i,x,y});
-				look.add(new Integer[] {(i+1)%4,x,y});
-				look.add(new Integer[] {(i+2)%4,x,y});
-				dfs(idx+1, look);
-				look.remove(look.size()-1);
-				look.remove(look.size()-1);
-				look.remove(look.size()-1);
-			}
-			break;
-		case 5: // 4방향
-			look.add(new Integer[] {0,x,y});
-			look.add(new Integer[] {1,x,y});
-			look.add(new Integer[] {2,x,y});
-			look.add(new Integer[] {3,x,y});
-			dfs(idx+1, look);
-			look.remove(look.size()-1);
-			look.remove(look.size()-1);
-			look.remove(look.size()-1);
-			look.remove(look.size()-1);
-			break;
 		}
 	}
 	
-	
-	private static void bfs(int dir, int x, int y) {// 한방향으로만 탐색
-		Deque<Integer[]> q = new ArrayDeque<Integer[]>();
-		q.offer(new Integer[] {x, y});
+	private static void spread(int x, int y, int dir) {
+		dir = dir%4;
 		
-		while (!q.isEmpty()) {
-			Integer[] temp = q.pollFirst();
-			x = temp[0];
-			y = temp[1];
-			
-			int nx = x+dx[dir];
-			int ny = y+dy[dir];
-			if (nx>=0 && nx<N && ny>=0 && ny<M) {
-				if (graph[nx][ny] == 6) {//벽 만나면 멈춤
-					break; 
-				}
-				else if (graph[nx][ny] == 0) {
-					newGraph[nx][ny] = -1; // 감시지역 -1
-					q.offer(new Integer[] {nx, ny});
-				}else if (graph[nx][ny] >= 1 && graph[nx][ny] <= 5) {// 카메라면 통과
-					q.offer(new Integer[] {nx, ny});
-				}
-			}else {
-				break;
+		Deque<int[]> queue = new ArrayDeque<>();
+		queue.add(new int[] {x,y});
+		
+		while (!queue.isEmpty()) {
+			int[] pos = queue.pollFirst();
+			int nx,ny;
+			nx = pos[0]+dx[dir];
+			ny = pos[1]+dy[dir];
+			if (nx<0||nx>=N||ny<0||ny>=M) continue;
+			if (tempMap[nx][ny] == 6) continue;
+			queue.add(new int[] {nx,ny});
+			tempMap[nx][ny] = 7; // 감시하는 곳 			
+		}
+	}
+
+	private static void copyMap() {
+		// TODO Auto-generated method stub
+		for (int i=0;i<N;i++) {
+			for (int j=0;j<M;j++) {
+				tempMap[i][j] = originalMap[i][j];
 			}
 		}
 	}
