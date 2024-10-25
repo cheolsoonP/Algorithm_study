8fc46261926aea0f62deb49489e6fc49dd18bf1a
Park Cheolsoon <mn5724@naver.com>
2024-10-25 11:12:35 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 1020 ms, Memory: 55004 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, M;
+	static Map<Integer, String> pocketMonNum;
+	static Map<String, Integer> pocketMonName;
+	
+	
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st; 
+		pocketMonNum = new HashMap<>(); 
+		pocketMonName = new HashMap<>(); 
+		
+		st = new StringTokenizer(in.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		for (int i=1;i<=N;i++) {
+			String name = in.readLine();
+			pocketMonNum.put(i, name);
+			pocketMonName.put(name, i);
+		}
+		
+		for (int i=1;i<=M;i++) {
+			String q = in.readLine();
+			if (q.charAt(0) >= '1' && q.charAt(0) <= '9') {
+				int num = Integer.parseInt(q);
+				System.out.println(pocketMonNum.get(num));
+			} else {
+				System.out.println(pocketMonName.get(q));
+			}
+			
+		}
+		
+	}
+}
