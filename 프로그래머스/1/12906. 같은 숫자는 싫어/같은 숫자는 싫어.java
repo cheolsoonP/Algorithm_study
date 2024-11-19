853cba46ff30d80d3c0b3a7c145bd91fffeedcde
Park Cheolsoon <mn5724@naver.com>
2024-11-19 11:38:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 37.84 ms, Memory: 110 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public Stack<Integer> solution(int []arr) {
+        Stack<Integer> stack = new Stack<>(); 
+        
+        for (int num : arr) {
+            if (stack.empty() || stack.peek() != num) {
+                stack.push(num); 
+            }
+        }
+        
+        return stack;
+    }
+}
