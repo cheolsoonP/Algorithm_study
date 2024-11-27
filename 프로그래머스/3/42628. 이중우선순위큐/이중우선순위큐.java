73b2ba2f1bc01b4ad05e65d02019e40a7beed56d
Park Cheolsoon <mn5724@naver.com>
2024-11-27 12:56:12 +0900

    Update 이중우선순위큐.java

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -5,93 +5,26 @@ class Solution {
     public int[] solution(String[] operations) {
         PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Comparator.reverseOrder()); 
         PriorityQueue<Integer> minQueue = new PriorityQueue<>(); 
-        Map<Integer, Integer> numCnt = new HashMap<>(); // num, cnt 
         
         StringTokenizer st; 
         for (String str : operations) {
-            st = new StringTokenizer(str); 
-            String cmd = st.nextToken(); 
-            int num = Integer.parseInt(st.nextToken());
-
-            if (cmd.equals("I")) {
+            int num = Integer.parseInt(str.substring(2));
+            if (str.startsWith("I")) {
                 // 값 추가 
                 maxQueue.add(num);
                 minQueue.add(num);
-                numCnt.put(num, numCnt.getOrDefault(num, 0)+1);
-            } else if (cmd.equals("D")) {
-                if (num == 1) {
-                    // 최대값 삭제
-                    int maxNum = -1; 
-                    while (true) {
-                        if (maxQueue.size() == 0) break;
-                        maxNum = maxQueue.peek();
-                        if (numCnt.get(maxNum) == 0) {
-                            maxQueue.poll();
-                        } else {
-                            break; 
-                        }
-                    }
-                    if (maxQueue.size() == 0) continue;
-                    maxQueue.poll();
-                    numCnt.put(maxNum, numCnt.get(maxNum)-1);
-                } else {
-                    // 최솟값 삭제 
-                    int minNum = -1;
-                    while (true) {
-                        if (minQueue.size() == 0) break;
-                        minNum = minQueue.peek();
-                        if (numCnt.get(minNum) == 0) {
-                            minQueue.poll();
-                        } else {
-                            break; 
-                        }
-                    }
-                    if (minQueue.size() == 0) continue;
-                    minQueue.poll();
-                    numCnt.put(minNum, numCnt.get(minNum)-1);
-                }
+            } else if (str.startsWith("D 1") && maxQueue.size() > 0) {
+                // 최대값 삭제
+                minQueue.remove(maxQueue.poll());
+            } else if (str.startsWith("D -1") && minQueue.size() > 0) {
+                // 최솟값 삭제 
+                maxQueue.remove(minQueue.poll());
             }
         }
-        
-        while(true) {
-            int num;
-            if (minQueue.size() > 0) {
-                num = minQueue.peek();
-                if (numCnt.get(num) == 0) {
-                    minQueue.poll();
-                } else {
-                    break; 
-                }
-            } else {
-                break; 
-            }
-        }
-        while(true) {
-            int num;
-            if (maxQueue.size() > 0) {
-                num = maxQueue.peek();
-                if (numCnt.get(num) == 0) {
-                    maxQueue.poll();
-                } else {
-                    break; 
-                }
-            } else {
-                break; 
-            }
-        }
-        
-        int[] res = new int[2]; 
-        if (maxQueue.size() == 0) {
-            res[0] = 0;
+        if (maxQueue.size() == 0 && minQueue.size() == 0) {
+            return new int[]{0,0};
         } else {
-            res[0] = maxQueue.peek(); 
+            return new int[]{maxQueue.peek(), minQueue.peek()};
         }
-        if (minQueue.size() == 0) {
-            res[1] = 0;
-        } else {
-            res[1] = minQueue.peek(); 
-        }
-        
-        return res; 
     }
 }
